56042c7f84fd27c029a9052d65d468e3c0ac5fdc
Gautier Levert <gautierlevert@gmail.com>
2017-03-29 23:09:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/levertg/butterknifeapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.levertg.butterknifeapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        textView.setText("view bound");
+    }
+
+    @OnClick(R.id.button)
+    void buttonClick() {
+        Log.d(MainActivity.class.getSimpleName(), "button clicked");
+    }
+}
